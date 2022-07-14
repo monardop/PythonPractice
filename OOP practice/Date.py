6870044372f1b80b7d11b85ef6b636cca0007dac
Pablo Monardo <pablomonardo@gmail.com>
2022-07-14 10:04:08 -0300

    Debugged and corrected its errors

--- OOP practice/Date.py
+++ OOP practice/Date.py
@@ -1,41 +1,40 @@
 class Date:
-    base_year = 1900
-    normal_days = (0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365)
-    leap_days = (0, 0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366)
+    base_year = 1601
+    amount_days = (( 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365),(0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366))
     normal_month = (0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
     leap_month = (0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 
     def __init__(self, day: int, month: int, year: int):
         if self.valid_date(day, month, year) == False:
-            print("Invalid date.")
-            return
+            return None
+            
         amountYears = year - self.base_year
         amountDays = amountYears*365 + int(amountYears/4) - int(amountYears/100) + int(amountYears/400)
         self.relative_days = amountDays + self.gregorian_days(day,month,year)
         self.day = day
         self.month = month
         self.year = year
-        print(f"Your date is {self.day}/{self.month}/{self.year}. There were {self.relative_days} days from 1900")
+        print(f"Your date is {self.day}/{self.month}/{self.year}. There were {self.relative_days} days from 1601")
 
     def gregorian_days(self, day: int, month: int, year: int) -> int:
-        if self.is_leap(year) == 1:
-            return self.normal_days[month] + day
-        else: 
-            return self.leap_days[month] + day
+        return self.amount_days[self.is_leap(year)][month] + day
+        
     def valid_date(self, day: int, month: int, year: int) -> bool:
         if year < self.base_year:
             print("Your year is too old.")
             return False
-        if month > 13 or month < 0:
+        if month > 12 or month < 1:
             print("Invalid month entry.")
             return False
         is_leap_year = self.is_leap(year)
-        if is_leap_year == 1: 
-            if day < 1 and day > self.leap_month[month]:
+        if is_leap_year == True: 
+            if day < 1 or day > self.leap_month[month]:
+                print(f"{self.leap_month[month]}")
                 print("Invalid days entry.")
                 return False
         else:
-            if day < 1 and day > self.normal_month[month]:
+            if day < 1 or day > self.normal_month[month]:
+                print(f"{self.normal_month[month]}")
                 print("Invalid days entry.")
                 return False
         return True
--- OOP practice/main.py
+++ OOP practice/main.py
@@ -1,3 +1,6 @@
 import Date 
 
 r = Date.Date(22,7,1997)
+f = Date.Date(2,13,1920)
+g = Date.Date(92,10,1950)
+i = Date.Date(12,4,92)
