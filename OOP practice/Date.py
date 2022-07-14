4455a9d224541e6ed3410f9806982b0b8c5bc37c
Pablo Monardo <pablomonardo@gmail.com>
2022-07-14 10:53:26 -0300

    Update for Date.py
    Added a way to obtain the date in terms of relative days (days passed from the base year)

--- OOP practice/Date.py
+++ OOP practice/Date.py
@@ -17,8 +17,7 @@ class Date:
         print(f"Your date is {self.day}/{self.month}/{self.year}. There were {self.relative_days} days from 1601")
 
     def gregorian_days(self, day: int, month: int, year: int) -> int:
-        return self.amount_days[self.is_leap(year)][month] + day
-        
+        return self.amount_days[self.is_leap(year)][month] + day        
     def valid_date(self, day: int, month: int, year: int) -> bool:
         if year < self.base_year:
             print("Your year is too old.")
@@ -38,5 +37,20 @@ class Date:
                 print("Invalid days entry.")
                 return False
         return True
-    def is_leap(self, year: int) -> int:
+    def is_leap(self, year: int) -> bool:
         return (year % 4 == 0 and (year%100 != 0 or year%400 == 0))
+    def getDMY(self, relDays:int) -> tuple:
+        years_passed = int(relDays/365)
+        years_completed = int(years_passed * 365 + years_passed / 4 - years_passed / 100 + years_passed / 400)
+        while years_completed >= relDays:
+            years_passed -= 1
+            years_completed = int(years_passed * 365 + years_passed / 4 - years_passed / 100 + years_passed / 400)
+        year = self.base_year + years_passed
+        years_day = relDays - years_completed
+        row = 1 if self.is_leap(year) else 0
+        month = 1
+        while years_day > self.amount_days[row][month]:
+            month += 1
+        day = years_day - self.amount_days[row][month - 1]
+        print(f"{(day, month, year)}")
+        return (day, month, year)
