80578a83dc4d77e20541bdc74014c21af34569e5
Pablo Monardo <pablomonardo@gmail.com>
2022-07-26 19:35:26 -0300

    Update time.py

--- OOP practice/time.py
+++ OOP practice/time.py
@@ -7,6 +7,8 @@ class Time:
         self.hour = hour
         self.minutes = minutes
         self.seconds = seconds
+        self.relTime = self.all_to_segs()
+    
     def all_to_segs(self) -> int:
         return ((self.hour * 3600) + (self.minutes * 60) + self.seconds)
     def hours_in_decimal(self) -> float:
@@ -15,4 +17,8 @@ class Time:
     def minutes_in_decimal(self) -> float:
         seconds = self.all_to_segs()
         return (seconds/60)
-    
+    def show_AMPM(self) -> str:
+        if self.hour < 12:
+            return f"{self.hour}:{self.minutes}:{self.seconds} AM"
+        else:
+            return f"{self.hour - 12}:{self.minutes}:{self.seconds} PM"
