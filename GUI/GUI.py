acf9de3e613fd49f654cd898acb91cbe11a37a35
Pablo Monardo <pablomonardo@gmail.com>
2022-08-09 19:08:39 -0300

    Update GUI.py

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -4,8 +4,16 @@ class App(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
         self.pack()
+        
+        self.lSide = tk.Label(self, text="Today", bg="gray", width=30)
+        self.lSide.pack()
+
+        self.new_task = tk.Button(self, text="New task", padx=10, pady= 5, bg="navy")
+        self.new_task.pack()
 
 if __name__ == "__main__":
     root = tk.Tk()
+    root.geometry("800x600")
     myapp = App(root)
+    myapp.master.title("To Do App")
     myapp.mainloop()
